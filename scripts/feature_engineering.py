a903ab94524e8cc009ade9d8d03092f1966fdaf8
ClintonBeyene <clintonbeyene52@gmail.com>
2024-10-02 13:19:36 +0300

    create new feature age_group based on age

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -20,3 +20,19 @@ def label_encoding(df):
     for column in columns:
         le = LabelEncoder()
         df[column] = le.fit_transform(df[column])
+
+    return df
+
+# Function to apply feature engineeing 
+def feature_enginerring(df):
+    # Create a new feature 'age_group' based on age
+    df['age_group'] = pd.cut(df['age'], bins=[0, 20, 40, 60], 
+                             labels=['young', 'adult', 'middle_age', 'old'])
+    # Convert 'age_group' to numerical variable using LabelEncoder
+    le = LabelEncoder()
+    df['age_group'] = le.fit_tranform(df['age_group'])
+
+    # Create a new feature 'contact_duration' based on 'duration'
+    df['contach_duration'] = df['duration'] / 60
+
+    return df
