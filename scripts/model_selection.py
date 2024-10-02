c264eb8f77c65f25a5fef82050b541054b3ad5ac
ClintonBeyene <clintonbeyene52@gmail.com>
2024-10-02 15:02:08 +0300

    import necessarry libraries

--- scripts/model_selection.py
+++ scripts/model_selection.py
@@ -1,17 +1,5 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import GridSearchCV
-from sklearn.decomposition import PCA
-from sklearn.metrics import roc_auc_score, roc_curve
-import shap
 from sklearn.model_selection import cross_val_score
 
 def model_selection(X_train, y_train):
